cb25da390015825fcfa184924ae5f89cb3c0f457
Ahmed Baligh <58535713+ahmedbaligh@users.noreply.github.com>
2021-06-02 22:05:59 +0200

    feat: Implement basic app structure
    Update app's name, logo, favicons, metadata.
    Add basic routing functionality.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
+import logo from './assets/logo.svg';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div>
+          <img src={logo} alt="" />
+
+          <div>
+            <ul>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/welcome">Welcome</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <Switch>
+          <Route exact path="/login">
+            Login
+          </Route>
+
+          <Route exact path="/welcome">
+            Welcome
+          </Route>
+
+          <Route path="*">Error 404</Route>
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
